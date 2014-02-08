347713107f630926d82089821e1cd22253d8e79b
sonofmun <mmunson@gcdh.de>
2014-02-08 19:53:08 +0100

    Finishing LL Calc for VSM file
    Almost finished doing the script for VSM LL calculations.  Having
    trouble with UTF strings on Windows.  Will try in Linux.

--- VSMCollLogLike.py
+++ VSMCollLogLike.py
@@ -6,18 +6,23 @@ Created on 25.01.2014
 import re
 import math
 import os.path
-import decimal
+import decimal as deci
 from tkinter.filedialog import askopenfilename
-from tkinter.filedialog import askdirectory
+from tkinter.filedialog import asksaveasfilename
 import numpy as np
-CollVSM = askopenfilename(title = 'Where is your collocate VSM located?')
-with open(CollVSM, encoding = 'utf-8', newline = '\n') as CollFile:
+CollVSMFile = askopenfilename(title = 'Where is your collocate VSM located?')
+LLVSMFile = asksaveasfilename(title = 'Where would you like to save your log likelihood VSM file?')
+with open(CollVSMFile, encoding = 'utf-8', newline = '\n') as CollFile:
     CollList = [x.rstrip('\n') for x in CollFile.readlines()]
-headings = [x.strip(" '") for x in CollList.pop(0).split(',')]
+headings = [x.strip(" '\r") for x in CollList.pop(0).split(',')]
 formats = ['<U30'] #initiates the list that will become the list of the object types of the columns, setting the type of the first column (the target word)
 [formats.append('int') for x in range(len(headings)-1)] #finishes creating the list of column object types, typing them all as floating point depending on the number of lexical types that exist in the original document.
-mydescriptor = {'names': headings, 'formats': testformats}
+mydescriptor = {'names': headings, 'formats': formats}
+LLformats = ['<U30', 'int']
+[LLformats.append('float') for x in range(len(headings)-1)]
+LLdescriptor = {'names': headings, 'formats': LLformats}
 CollList = [x.split(', ') for x in CollList] #this separates each element in CollList (each of which are str) into individual elements
+CountDict = {}
 #the following loop transforms each numerical element in CollList into an int type
 for i, line in enumerate(CollList):
     for i2, element in enumerate(line):
@@ -27,52 +32,47 @@ for i, line in enumerate(CollList):
             line[i2] = element.strip("'")
             continue
     CollList[i] = tuple(line)
-VSM = np.array(CollList, dtype = mydescriptor)
-decimal.getcontext().Emin = -425000000
-for file in FileList:    #iterates through the collcation files in the source directory
-    LLCollList = ''
-    Lemma = re.sub(r'Coll_([^\']+)_[^_]+_NP4.txt', r'\1', file)
-    LemCount = lemlist.count(Lemma)
-    CollFilename = CollDir + '/' + file #sets the absolute path for the source file
-    CollFile = open(CollFilename, mode = 'r', encoding = 'utf-8') #opens the source file
-    LLfile = re.sub(r'.txt', r'_LL.txt', file) #sets the name of the destination file dependent on the name of the source file
-    LLFileName = CollDir + "/LL/" + LLfile #sets the absolute path for the destination file
-    if os.path.isfile(LLFileName) == True: #checks to see if a collocation file already exists for this lemma
-        continue #if a collocation file for this lemma exists, it breaks out of the if loop and selects the next member of lemlist
-    else: #this triggers if the significance file does not yet exist
-        LLListFile = open(LLFileName, mode = 'w', encoding = 'utf-8') #opens the destination file for writing in utf-8
-        for line in CollFile: # iterates through each line in the source file
-            line = line.strip('\'\n') # = re.sub(r'\"', '', line, count = 1)
-            if 'Lemma' in line:
-                line = re.sub(r'\' ', r' ', line)
-                LLLem = line + ' LL=NA\n'
-                LLCollList = LLCollList + LLLem
-                continue
-            else:
-                line = re.sub(r'\' ', r' ', line)
-                CollCount = int(re.sub(r'[^\']+ ([0-9]+)', r'\1', line)) #extracts the number of co-occurrences
-                CollLemCount = lemlist.count(re.sub(r'([^\']+) [0-9]+', r'\1', line)) #extracts the total number of occurrences of the lemma from the corpus.                Num = len(lemlist) #calculates the size of the corpus
-                C1 = decimal.Decimal(LemCount) #this is the number of occurrences of the word under investigation
-                C2 = decimal.Decimal(CollLemCount) #this is the number of times the co-occurrent occurs in the corpus
-                C12 = decimal.Decimal(CollCount/8) #this is the number of times the original word and the co-occurrent occur together.  I divide by 8 because the number of occurrences is spread over a collocation window 8 words wide.
-                P = decimal.Decimal(C2/Num) #the probability that a random word in the corpus will be the co-occurrent
-                P1 = decimal.Decimal(C12/C1) #the probability that the co-occurrent will occur with the original lemma
-                P2 = decimal.Decimal((C2 - C12)/(Num-C1)) #the probability that the co-occurrent occurs apart from the original lemma (I think)
-                Test = C2-C12
-                LL1 = decimal.log10(decimal.power(P, C12)*decimal.power(1-P, C1-C12))
-                LL2 = decimal.log10(decimal.power(P, C2-C12)*decimal.power(1-P, Num-C1-C2-C12))
-                if P1 == 1:
-                    LL3 = 0 #I need to do this because otherwise decimal throws and error when it takes 0 to the 0 power
-                else:
-                    LL3 = decimal.log10(decimal.power(P1, C12)*decimal.power(1-P1, C1-C12))
-                if P2 == 0:
-                    LL4 = 0 #I need to do this because otherwise decimal throws and error when it takes 0 to the 0 power
-                else:
-                    LL4 = decimal.log10(decimal.power(P2, C2-C12)*decimal.power(1-P2, (Num-C1)-(C2-C12)))
-                LL = -2*(LL1+LL2-LL3-LL4) #this is the calculation of the log likelihood adjusted to be referenced on a chi-squared table (i.e., *-2)
-                LLColl = line + ' ' + str(LL) + '\n'
-                LLCollList = LLCollList + LLColl
-        LLCollStr = str(LLCollList)
-        LLListFile.write(LLCollStr)
-        LLListFile.close()
-lemdoc.close()
+    CountDict[CollList[i][0]] = CollList[i][1]
+CollVSM = np.array(CollList, dtype = mydescriptor)
+LLVSM = np.zeros(len(CollList), dtype = LLdescriptor)
+deci.getcontext().Emin = -425000000
+for i2, element in enumerate(CollVSM):
+    element = tuple(element)
+    Lemma = element[0]
+    LemCount = element[1].astype(deci.Decimal)
+    LLVSM[i2][0] = Lemma
+    LLVSM[i2][1] = LemCount
+    for i, CollCount in enumerate(element[2:]): # iterates through each line in the source file
+        Collocate = headings[i+2]
+        CollLemCount = CountDict[Collocate]
+        Num = sum(CountDict.values())
+        C1 = deci.Decimal(LemCount) #this is the number of occurrences of the word under investigation
+        C2 = deci.Decimal(CollLemCount) #this is the number of times the co-occurrent occurs in the corpus
+        C12 = deci.Decimal(CollCount.astype(deci.Decimal)/8) #this is the number of times the original word and the co-occurrent occur together.  I divide by 8 because the number of occurrences is spread over a collocation window 8 words wide.
+        P = deci.Decimal(C2/Num) #the probability that a random word in the corpus will be the co-occurrent
+        P1 = deci.Decimal(C12/C1) #the probability that the co-occurrent will occur with the original lemma
+        P2 = deci.Decimal((C2 - C12)/(Num-C1)) #the probability that the co-occurrent occurs apart from the original lemma (I think)
+        Test = C2-C12
+        LL1 = deci.Decimal.log10(deci.Context().power(P, C12)*deci.Context().power(1-P, C1-C12))
+        LL2 = deci.Decimal.log10(deci.Context().power(P, C2-C12)*deci.Context().power(1-P, Num-C1-C2-C12))#
+        #if P1 == 1:
+            #LL3 = 0 #I need to do this because otherwise decimal throws and error when it takes 0 to the 0 deci.power
+        #elif P1 == 0:
+        #    LL3 = 0
+        #else:
+        try:
+            LL3 = deci.Decimal.log10(deci.Context().power(P1, C12)*deci.Context().power(1-P1, C1-C12))
+        except deci.InvalidOperation as E:
+            LL3 = 0
+            continue
+        #if P2 == 0:
+        #    LL4 = 0 #I need to do this because otherwise decimal throws and error when it takes 0 to the 0 deci.power
+        try:
+            LL4 = deci.Decimal.log10(deci.Context().power(P2, C2-C12)*deci.Context().power(1-P2, (Num-C1)-(C2-C12)))
+        except deci.InvalidOperation as E:
+            LL4 = 0
+            continue
+        LL = -2*(LL1+LL2-LL3-LL4) #this is the calculation of the log likelihood adjusted to be referenced on a chi-squared table (i.e., *-2)
+        LLVSM[i2][i+2] = LL
+with open(LLVSMFile, mode = 'w', encoding = 'utf-8') as LLFile:
+    np.savetxt(LLFile, LLVSM, delimiter = ',', fmt = '%s')
