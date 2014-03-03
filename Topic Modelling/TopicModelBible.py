061d06c437967138b8171c3426e44b5380fb3245
sonofmun <mmunson@gcdh.de>
2014-03-03 09:21:36 +0100

    Move some files

--- Topic Modelling/TopicModelBible.py
+++ Topic Modelling/TopicModelBible.py
@@ -3,11 +3,11 @@ import numpy as np
 import sklearn.feature_extraction.text as text
 from sklearn import decomposition
 from decimal import *
-with open(r'C:\Users\matthew.munson\Google Drive\Dissertation\CompLing\GBible\Greek Stop Words (Digital Classicist Wiki).txt', mode = 'r', encoding = 'utf-8') as StopFile:
-    StopList = StopFile.read().split(', ')
-paths = paths = [r'C:\Users\matthew.munson\Google Drive\Dissertation\CompLing\GBible\UTF8\ChunkedNT']#, r'C:\Users\matthew.munson\Google Drive\Dissertation\CompLing\GBible\UTF8\ChunkedLXX']
+#with open(r'C:\Users\matthew.munson\Google Drive\Dissertation\CompLing\GBible\Greek Stop Words (Digital Classicist Wiki).txt', mode = 'r', encoding = 'utf-8') as StopFile:
+#    StopList = StopFile.read().split(', ')
+paths = paths = [r'C:\CompLing\HBible']#, r'C:\Users\matthew.munson\Google Drive\Dissertation\CompLing\GBible\UTF8\ChunkedLXX']
 filenames = sorted([os.path.join(path, fn) for path in paths for fn in os.listdir(path) if fn.endswith('.txt')])
-vectorizer = text.CountVectorizer(input='filename', stop_words = StopList, min_df=20)
+vectorizer = text.CountVectorizer(input='filename', min_df=20) #stop_words = StopList, min_df=20)
 dtm = vectorizer.fit_transform(filenames).toarray()
 vocab = np.array(vectorizer.get_feature_names())
 num_topics = 20
@@ -18,7 +18,7 @@ topic_words = []
 for topic in clf.components_:
     word_idx = np.argsort(topic)[::-1][0:num_top_words]
     topic_words.append([[vocab[i], float(round(Decimal(topic[i]), 5))] for i in word_idx])
-with open(r'C:\Users\matthew.munson\Google Drive\Dissertation\CompLing\GBible\UTF8\NTTops.txt', mode = 'w+', encoding = 'utf-8') as TopicFile:
+with open(r'C:\CompLing\HBible\HBTops.txt', mode = 'w+', encoding = 'utf-8') as TopicFile:
     for t in range(len(topic_words)):
         TopicFile.write("Topic {}: {}\n".format(t, ''.join(str(topic_words[t]).replace(r'[', '').replace(r']', ''))))
 
