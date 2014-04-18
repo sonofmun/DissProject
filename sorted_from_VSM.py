7f6f1eabe031ad50d31a10bfb134a6f49b79a1b9
sonofmun <mmunson@gcdh.de>
2014-04-18 17:36:16 +0200

    fixed another small error in conversion to abridged

--- sorted_from_VSM.py
+++ sorted_from_VSM.py
@@ -6,7 +6,8 @@ VSM_list = os.listdir(orig_dir)
 for file in VSM_list:
     if not file.endswith('.txt'):
         VSM_list.remove(file)
-for filename in VSM_list:
+for file in VSM_list:
+    filename = os.path.join(orig_dir, file)
     dest_filename = ''.join([os.path.splitext(filename)[0], '_Abridged', os.path.splitext(filename)[1]])
     CS, lem_dict = Txt_to_nparray(filename)
     unsorted_dict = {}
