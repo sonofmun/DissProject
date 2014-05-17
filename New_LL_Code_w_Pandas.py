e96fd95f944cba99776bc2196ad4a55b09204512
sonofmun <mmunson@gcdh.de>
2014-05-17 18:10:32 +0200

    Edits to Fisher exact code

--- New_LL_Code_w_Pandas.py
+++ New_LL_Code_w_Pandas.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 from tkinter.filedialog import askdirectory
 import os.path
+from scipy.stats import chi2_contingency
 def file_dict_builder():
     orig_dir = askdirectory(title= 'Where are your collocate DataFrames and lem_dicts located?')
     dest_dir = askdirectory(title = 'Where would you like to save the resulting files?')
@@ -17,24 +18,28 @@ def file_dict_builder():
             df_dict_dict[file] = ''.join([file.split('.')[0], '_lem_dict.pickle'])
     return df_dict_dict, dest_dir
 def log_like(x):
-    #The log-likelihood calculator to be called by df.apply()
+    #For chi2 contingency test, we need first to construct a 2x2 contingency table like the one below
+    #      |    w2    |    -w2    |
+    #------------------------------
+    #  w1  | c(w1,w2) | c(w1,-w2) |  c(w1)
+    #--------------------------------------
+    #  -w1 | c(-w1,w2)|c(-w1,-w2) |  c(-w1)
+    #--------------------------------------
+    #      |   c(w2)  | c(-w2)    |   N
+    #
+    #c(w1,w2) is the value in the cell in Series x that we are investigating
+    #c(w1,-w2) is c(w1)-c(w1,w2).  c(w1) is extracted by looking up the word that is the 'name' of the Series x and finding its value in the lem_counts Series
+    #c(-w1,w2) is c(w2)-c(w1,w2).  c(w2) is the cell in lem_counts that matches the cell we are dealing with now, i.e., has the same index.
+    #Thus, c(w2) can be accessed simply by calling lem_counts since all calculations will be vectorized and thus matched.
+    #c(-w1,-w2) is either c(-w2)-c(w1,-w2) or c(-w1)-c(-w1,w2).
+    #And since c(-w2) (or -w1) is N-c(w2), we can rewrite this as N-c(w2)-c(w1,-w2).
+    #And since c(w1,-w2) is c(w1)-c(w1,w2), then this can be rewritten as N-c(w2)-(c(w1)-c(w1,w2)) or N-c(w2)-c(w1)+c(w1,w2).
+    #Adding c(w1,w2) back at the end corrects for the number of times that w1 appears with w2 that were already counted in c(w2)
     c1 = lem_counts[x.name] #the count of the target word (names of the row in the df)
-    #In the code below, the following entities map in the following way to the variables in
-    #Manning and Schütze (1999) p. 173
-    #lem_counts = c2, i.e., the count of the collocate, extracted from the lem_counts series
-    #N = N, the total number of words in the corpus
-    #c1 = c1, explained above
-    #x = c12, the number of co-occurrences of the co-occurrent with the target word, extracted from the Coll_df DataFrame
-    #p in Manning & Schütze is lem_counts/N, i.e., the probability of the co-occurrent appearing in the text
-    #p1 in Manning & Schütze is x/c1, i.e., the probability of the two words co-occurring given that the target word appears,
-    #   i.e., the % of time word 2 occurs out of the total occurrences of word 1
-    #p2 in Manning & Schütze is (lem_counts-x)/(N-c1), i.e., the probabilty that word 2 appears outside of co-occurrence with the target word
-    L1 = np.log(((lem_counts/N)**c1)*((1-lem_counts/N)**(c1-x)))
-    L2 = np.log(((lem_counts/N)**(lem_counts-x))*((1-lem_counts/N)**((N-c1)-lem_counts-x)))
-    L3 = np.log(((x/c1)**x)*((1-x/c1)**(c1-x)))
-    L4 = np.log((((lem_counts-x)/(N-c1))**(lem_counts-x))*((1-(lem_counts-x)/(N-c1))**((N-c1)-lem_counts-x)))
-    LL = L1+L2-L3-L4
-    return LL
+    LL, p, dof, expected = chi2_contingency([[x, max(c1-x, 0)],[max(lem_counts-x,0), max(N-lem_counts-c1+x,0)]], lambda_ = 0)
+    return LL, p
+#The above returns one value for the whole row.  I need to figure out how to get it to return a value for each element.
+
 def counter(lem_dict_filename):
     #constructs a series of the total counts of all of the lemmata from the lem_dict
     #returns this series and N, the total number of words in the corpus
@@ -48,8 +53,10 @@ def counter(lem_dict_filename):
 
 file_dict, dest_dir = file_dict_builder()
 for df_file, lem_file in file_dict.items():
-    dest_file = ''.join([dest_dir, df_file.split('_')[0], '_LL.pickle'])
+    LL_dest_file = ''.join([dest_dir, df_file.split('_')[0], '_LL.pickle'])
+    LL_p_dest_file = ''.join([dest_dir, df_file.split('_')[0], '_LL_p.pickle'])
     lem_counts, N = lem_counter(lem_file)
     Coll_df = pd.read_pickle(df_file)
-    LL_df = Coll_df.apply(lambda x: log_like(x), axis = 1)
-    LL_df.to_pickle(dest_file)
+    LL_df, LL_p_df = Coll_df.apply(lambda x: log_like(x), axis = 1)
+    LL_df.to_pickle(LL_dest_file)
+    LL_p_df.to_pickle(LL_p_dest_file)
