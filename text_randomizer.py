ce4d7ed6b70444cec2c9b5376ac625c626644108
sonofmun <mmunson@gcdh.de>
2015-03-13 14:34:03 +0100

    Removed all the old unnecessary code. If needed, it can be found on the "old" branch.

--- text_randomizer.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'matt'
-
-def randomizer(l):
-    """Takes a list of words as input, randomizes the list 1000 times,
-    and returns the randomized list.
-    """
-    from numpy.random import shuffle
-    [shuffle(l) for x in range(1000)]
-    return l
