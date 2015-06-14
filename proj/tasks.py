da1dc8925fd15190d07b8a476422022fd4f95e3b
sonofmun <mmunson@gcdh.de>
2015-06-14 11:14:47 +0200

    Started implementing unit tests Changed LouwNidaCatSim so that the synonyms for all words are only calculated once instead of for every window.

--- proj/tasks.py
+++ proj/tasks.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 
 import datetime
-import pandas as pd
 from celery import Celery
 from collections import defaultdict, Counter
 
@@ -50,24 +49,3 @@ def counter(weighted, w, words, limits):
 			print('Processing token {0} of {1} for window size {2} at {3}'.format(i, len(words), w,
 							datetime.datetime.now().time().isoformat()))
 	return Counter(cooc_dict)
-
-	'''s = pd.Series(0, index=[t], name=t)
-	if weighted:
-		for pos in range(max(i-w, 0),min(i+w+1, len(words))):
-			if pos != i:
-				for x in range(w+1-abs(i-pos)):
-					try:
-						s[words[pos]] += 1
-					except KeyError:
-						s[words[pos]] = 1
-	else:
-		for pos in range(max(i-w, 0),min(i+w+1, len(words))):
-			if pos != i:
-				try:
-					s[words[pos]] += 1
-				except KeyError:
-					s[words[pos]] = 1
-	if i % 100000 == 0:
-		print('Processing token %s of %s at %s' % (i, len(words),
-						datetime.datetime.now().time().isoformat()))
-	return s'''
