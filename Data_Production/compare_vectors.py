3675b1d2ba2a3daf34119c222f00d36910079c53
sonofmun <mmunson@gcdh.de>
2016-05-23 09:54:10 +0200

    Corrected a small error in cmd()

--- Data_Production/compare_vectors.py
+++ Data_Production/compare_vectors.py
@@ -196,7 +196,7 @@ def cmd():
     parser.add_argument('--english', type=str, help='The transliteration into Latin characters for the word under investigation')
     parser.add_argument('--greek', type=str, help='The word under investigation in its native alphabet')
     parser.add_argument('--measure', type=str, default='CS', choices=['CS', 'LL', 'PPMI', 'cooc'], help='The type of data to be used for the comparison')
-    parser.add_argument('--norm', type='norm', action='store_true', help='Whether to run data normalization on the input matrices (should be True if the data has not yet been normalized')
+    parser.add_argument('--norm', dest='norm', action='store_true', help='Whether to run data normalization on the input matrices (should be True if the data has not yet been normalized')
     parser.set_defaults(func=comparison, norm=False)
     args = parser.parse_args()
     pipe = args.func(**vars(args))
